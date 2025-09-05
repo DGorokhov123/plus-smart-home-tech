006fdf62eb14020c494345cdf0aa7d9bcef63377
Dmitriy Gorokhov <dgorokhov123@gmail.com>
2025-09-05 18:06:35 +0300

    Added grpc controller, service support and protobuf mappers

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/EventServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/EventServiceImpl.java
@@ -10,6 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.dto.hub.HubEvent;
 import ru.yandex.practicum.dto.sensor.SensorEvent;
+import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+import ru.yandex.practicum.mapper.GrpcHubEventMapper;
+import ru.yandex.practicum.mapper.GrpcSensorEventMapper;
 import ru.yandex.practicum.mapper.HubEventMapper;
 import ru.yandex.practicum.mapper.SensorEventMapper;
 
@@ -26,6 +30,8 @@ public class EventServiceImpl implements EventService {
     @Value("${smart-home-tech.kafka.sensor-event-topic}")
     private String sensorEventTopicName;
 
+    // SENSOR EVENTS
+
     @Override
     public ResponseEntity<Void> handleSensorEvent(SensorEvent sensorEvent) {
         log.debug("Received: {}", sensorEvent);
@@ -37,6 +43,17 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
+    public void handleSensorEvent(SensorEventProto sensorEventProto) {
+        log.debug("Received: {}", sensorEventProto);
+        SpecificRecordBase avroSensorEvent = GrpcSensorEventMapper.toAvro(sensorEventProto);
+        ProducerRecord<Void, SpecificRecordBase> record = new ProducerRecord<>(sensorEventTopicName, avroSensorEvent);
+        producer.send(record);
+        log.debug("Sent: {}", avroSensorEvent);
+    }
+
+    // HUB EVENTS
+
+    @Override
     public ResponseEntity<Void> handleHubEvent(HubEvent hubEvent) {
         log.debug("Received: {}", hubEvent);
         SpecificRecordBase avroHubEvent = HubEventMapper.toAvro(hubEvent);
@@ -44,6 +61,15 @@ public class EventServiceImpl implements EventService {
         producer.send(record);
         log.debug("Sent: {}", avroHubEvent);
         return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public void handleHubEvent(HubEventProto hubEventProto) {
+        log.debug("Received: {}", hubEventProto);
+        SpecificRecordBase avroHubEvent = GrpcHubEventMapper.toAvro(hubEventProto);
+        ProducerRecord<Void, SpecificRecordBase> record = new ProducerRecord<>(hubEventTopicName, avroHubEvent);
+        producer.send(record);
+        log.debug("Sent: {}", avroHubEvent);
     }
 
 }
